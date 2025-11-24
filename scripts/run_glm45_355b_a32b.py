152c50a46bb283fb5ca6b5e353f1d541e4aa7440
fzyzcjy <5236035+fzyzcjy@users.noreply.github.com>
2025-11-24 15:56:20 +0800

    Super tiny use dapo in glm demo (#245)

--- scripts/run_glm45_355b_a32b.py
+++ scripts/run_glm45_355b_a32b.py
@@ -26,7 +26,7 @@ class ScriptArgs(U.ExecuteTrainConfig):
     rollout_fp8: bool = False
     dynamic_sampling: bool = False
     # TODO use more complex task
-    task: Literal["dapo_aime", "gsm8k"] = "gsm8k"
+    task: Literal["dapo_aime", "gsm8k"] = "dapo_aime"
 
 
 @app.command()
