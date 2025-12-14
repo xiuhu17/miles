a414fcb855640602b82b48de912f021192019f38
Yueming Yuan <yy28@illinois.edu>
2025-12-13 19:06:18 -0800

    add several fix, supported thd + CP on megatron's dsa, added dockerfile

--- miles/backends/megatron_utils/loss.py
+++ miles/backends/megatron_utils/loss.py
@@ -719,7 +719,7 @@ def loss_function(
 
     return (
         loss,
-        num_tokens if args.calculate_per_token_loss else 1,
+        num_tokens if args.calculate_per_token_loss else torch.tensor(1, dtype=torch.int, device=logits.device),
         {
             "keys": list(log.keys()),
             "values": torch.tensor(
