19c34ef40353212c11a7c615db89ac4b9b06dccf
zhihaow6 <zhihaow6@illinois.edu>
2026-06-18 20:37:55 -0700

    update
    Signed-off-by: zhihaow6 <zhihaow6@illinois.edu>

--- miles_plugins/models/deepseek_v4/ops/kernel/fp4_act_quant.py
+++
@@ -1,9 +0,0 @@
-# DEPRECATED: contents moved to act_quant.py.
-#
-# The FP4 (E2M1) quant kernel + `fp4_act_quant` wrapper now live in
-# `miles_plugins.models.deepseek_v4.ops.kernel.act_quant`. The other kernels that
-# used to live here (fp8_gemm / fp4_gemm / sparse_attn / hc_split_sinkhorn) were
-# unused dead code and have been dropped.
-#
-# This file is intentionally empty and can be deleted (`rm` was blocked in the
-# environment that performed the migration).
